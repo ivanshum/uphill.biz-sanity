9fed17f0f0ddeaae8dd55b9c1e564050e988c365
ivanshum <ivanshummi@gmail.com>
2024-01-20 23:56:01 +0600

    init

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '0xf7j2sv',
+    dataset: 'production'
+  }
+})
---
+++ schemas/page.ts
@@ -0,0 +1,12 @@
+export default {
+    name: 'page',
+    type: 'document',
+    title: 'Page',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      }
+    ]
+  }
