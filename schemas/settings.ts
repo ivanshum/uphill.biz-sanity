9407abb08e78ca73e823413773c19373b97209c9
ivanshum <ivanshummi@gmail.com>
2024-01-22 05:20:00 +0600

    test schema

--- schemas/settings.ts
+++ schemas/settings.ts
@@ -2,11 +2,26 @@ export default {
     name: 'settings',
     type: 'document',
     title: 'Settings',
+    groups: [{
+      name:'all-fields',
+      title:'MyTitle',
+      default: true
+    },{
+      name:'regular',
+      title:'Regular',
+    }], 
     fields: [
       {
         name: 'sitetitle',
         type: 'string',
-        title: 'Site Title',        
+        title: 'Site Title',  
+        group: 'all-fields'      
+      },
+      {
+        name: 'sitetitle2',
+        type: 'string',
+        title: 'Site Title',  
+        group: 'regular'      
       }
     ]
   }
