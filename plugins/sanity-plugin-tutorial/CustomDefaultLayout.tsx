0c780ef7f37501021f1bcf5f715d44bf5833d5f9
ivanshum <ivanshummi@gmail.com>
2024-01-21 18:41:44 +0600

    remove tutorial

--- plugins/sanity-plugin-tutorial/CustomDefaultLayout.tsx
+++ plugins/sanity-plugin-tutorial/CustomDefaultLayout.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import {LayoutProps, useCurrentUser} from 'sanity'
-import {GetStartedTutorial} from './GetStartedTutorial'
+import {LayoutProps} from 'sanity'
 
 export function CustomDefaultLayout(props: LayoutProps) {
-  const user = useCurrentUser()
-
-  const showTutorial = Boolean(user?.roles?.length)
 
   return (
     <>
-      {showTutorial && <GetStartedTutorial />}
       {props.renderDefault(props)}
     </>
   )
